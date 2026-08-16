561e2c4a9bc58b756a9356707ecffc14e60d4099
acpasa91-cloud <acpasa91@gmail.com>
2026-08-17 01:44:36 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+from highrise import BaseBot, User, Position
 
+
+class Bot(BaseBot):
+
+    async def on_user_join(self, user: User, position: Position) -> None:
+        await self.highrise.chat(
+            f"سلام {user.username} 👋 به روم خوش اومدی! ❤️"
+        )
+
+    async def on_chat(self, user: User, message: str) -> None:
+        msg = message.lower().strip()
+
+        if msg == "!سلام":
+            await self.highrise.chat(
+                f"سلام {user.username} 😎🤝"
+            )
+
+        elif msg == "!help":
+            await self.highrise.chat(
+                "دستورات بات: !سلام | !help 🤖"
+            )
